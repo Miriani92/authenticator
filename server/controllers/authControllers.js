c103a21438ae954180973ce28cfaddca2a906245
Miriani92 <mac@Miriani.local>
2023-12-18 06:24:56 +0400

    add models

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,18 @@
 import { CustomError } from "../errors/customError.js";
 
-export const signup = async () => {};
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const isDataProvided = !name || !email || !password;
+  const isRegistered = await User.findOne({ email });
+  if (!isDataProvided) {
+    throw new CustomError.BadRequestError(
+      "Please provide name, email, password"
+    );
+  }
+  if (isRegistered) {
+    throw new CustomError.BadRequestError("Already registered");
+  }
+};
 export const login = async (req, res) => {};
-export const logout = async () => {};
-export const verifyAccountByEmail = async () => {};
+export const logout = async (req, res) => {};
+export const verifyAccountByEmail = async (req, res) => {};
---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (clientProvidedPassword) {
+  const isMatch = await bcrypt.compare(clientProvidedPassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
